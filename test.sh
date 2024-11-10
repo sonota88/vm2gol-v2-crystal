eacf946a000f92600b4a399fb174f535af55993c
sonota88 <yosiot8753@gmail.com>
2024-11-10 09:41:43 +0900

    Change extension: .vg.txt => .mrcl (test.sh, test_common)

--- test.sh
+++ test.sh
@@ -126,7 +126,7 @@ test_lex_nn() {
 
   echo "case ${nn}"
 
-  local input_file="${TEST_COMMON_DIR}/lex/${nn}.vg.txt"
+  local input_file="${TEST_COMMON_DIR}/lex/${nn}.mrcl"
   local temp_tokens_file="${TEMP_DIR}/test.tokens.txt"
   local exp_file="${TEST_COMMON_DIR}/lex/exp_${nn}.txt"
 
@@ -160,7 +160,7 @@ test_parse_nn() {
 
   echo "case ${nn}"
 
-  local input_file="${TEST_COMMON_DIR}/parse/${nn}.vg.txt"
+  local input_file="${TEST_COMMON_DIR}/parse/${nn}.mrcl"
   local temp_tokens_file="${TEMP_DIR}/test.tokens.txt"
   local temp_vgt_file="${TEMP_DIR}/test.vgt.json"
   local exp_file="${TEST_COMMON_DIR}/parse/exp_${nn}.vgt.json"
@@ -230,7 +230,7 @@ test_compile_nn() {
   local exp_file="${TEST_COMMON_DIR}/compile/exp_${nn}.vga.txt"
 
   echo "  lex" >&2
-  run_lex ${TEST_COMMON_DIR}/compile/${nn}.vg.txt \
+  run_lex ${TEST_COMMON_DIR}/compile/${nn}.mrcl \
     > $temp_tokens_file
   if [ $? -ne 0 ]; then
     ERRS="${ERRS},compile_${nn}_lex"
